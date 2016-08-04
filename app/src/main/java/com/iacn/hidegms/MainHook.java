36ba5d8cba9601da1a196a78957297f950d8caa9
meolunr <meolunr@gmail.com>
2016-08-04 16:51:48 +0800

    Add MainHook

---
+++ app/src/main/java/com/iacn/hidegms/MainHook.java
@@ -0,0 +1,15 @@
+package com.iacn.hidegms;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by iAcn on 2016/8/4
+ * Emali iAcn0301@foxmail.com
+ */
+public class MainHook implements IXposedHookLoadPackage{
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+
+    }
+}
